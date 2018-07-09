441c4c45d0b024251bf46a1f9cdb118a829c2be3
Lasni <grega.lasnibat@gmail.com>
2018-07-09 15:40:59 +0200

    setCustomParameters for googleAuthProvider

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -14,6 +14,7 @@ firebase.initializeApp(config)
 const database = firebase.database()
 
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider()
+googleAuthProvider.setCustomParameters({ prompt: 'select_account' })
 
 export { firebase, googleAuthProvider, database as default }
 
